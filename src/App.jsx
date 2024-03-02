93f7dac4ea0341f0ec5f1cd951b32def01b6983e
Lukas <lukkotecki@gmail.com>
2024-03-03 00:27:13 +0100

    mergin blank branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 function App() {
+<<<<<<< Updated upstream
   
   return (
     <>
       <div>
         to jeest zawartosc
+=======
+
+  return (
+    <>
+      <div>
+        To jest zawartosc
+>>>>>>> Stashed changes
       </div>
     </>
   )
